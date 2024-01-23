5d308ef7988a5a4ec45e91127e65140b09bed9a9
Brandon Stiffler <BrandonSt@beckhoff.com>
2024-01-23 13:41:38 -0500

    added tc build and run powershell script

---
+++ tc_build_run.ps1
@@ -0,0 +1,37 @@
+## Automated PLC build and run
+## Dependencies: 
+##  - Visual Studio 2019
+##  - TwinCAT XAE
+
+# instantiate DTE object - this is the Visual Studio automation COM library
+$dte = new-object -com "VisualStudio.DTE"
+# suppress VS interface
+$dte.SuppressUI = $true
+
+# paths
+$slnPath = "$pwd\.."
+
+# open solution file
+$sln = $dte.Solution
+echo "Opening solution in VS (background)..."
+$sln.Open("$slnPath\TC_ADO_Demo_PLC.sln")
+
+#### build options ###
+
+# get base TwinCAT project
+$systemProject = $sln.Projects.Item(1)
+
+# set build configuration
+$buildConfig = "Release|TwinCAT RT (x64)"
+
+# build specific project
+$sln.SolutionBuild.BuildProject($buildConfig, $systemProject.FullName, $true)
+
+# set run mode
+$systemManager = $systemProject.Object
+$systemManager.ActivateConfiguration()
+$systemManager.StartRestartTwinCAT()
+
+# close VS
+echo "Quitting VS (background)..."
+$dte.Quit()
